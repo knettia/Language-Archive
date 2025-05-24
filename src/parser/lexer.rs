b1cf23add064c7c6c07a43f298001bc120b35b7c
A. C. Gäßler <debian.valken@protonmail.com>
2025-05-24 14:38:20 +0300

    Created parser module base

---
+++ src/parser/lexer.rs
@@ -0,0 +1,221 @@
+use std::collections::VecDeque;
+
+use crate::data::syms::Symbol;
+use crate::data::ops::*;
+// use crate::data::;
+use super::token::*;
+
+// enum SyntaxType
+// {
+// 	CLike,
+// 	RustLike
+// }
+
+// struct Options
+// {
+// 	syntax_type: SyntaxType
+// }
+
+// enum Result
+// {
+// 	Success
+// }
+
+// struct Output
+// {
+// 	result: Result,
+// 	string: String,
+// 	tokens: Vec<Token>
+// }
+
+pub fn lex(data: String) -> VecDeque<Token>
+{
+	let mut chars = data.chars().peekable();
+
+	let mut line: i16 = 1;
+	let mut column: i16 = 1;
+
+	let mut tokens: VecDeque<Token> = VecDeque::new();
+
+	while let Some(c) = chars.next()
+	{
+		let info = TokenInfo
+		{
+			line,
+			column
+		};
+
+		column += 1;
+
+		match c
+		{
+			' ' | '\t' =>
+			{
+				continue;
+			}
+
+			'\n' =>
+			{
+				line += 1;
+				column = 1;
+			}
+
+			'+' =>
+			{
+				tokens.push_back(Token::new_arithmetic(info, ArithmeticOperation::Add));
+			}
+
+			'-' =>
+			{
+				tokens.push_back(Token::new_arithmetic(info, ArithmeticOperation::Subtract));
+			}
+
+			'*' =>
+			{
+				tokens.push_back(Token::new_arithmetic(info, ArithmeticOperation::Multiply));
+			}
+
+			'/' =>
+			{
+				tokens.push_back(Token::new_arithmetic(info, ArithmeticOperation::Divide));
+			}
+
+			'0'..='9' =>
+			{
+				let mut num = String::new();
+				num.push(c);
+
+				while let Some(current) = chars.peek()
+				{
+					if !current.is_ascii_digit()
+					{
+						break;
+					}
+
+					num.push(*current);
+					chars.next();
+					column += 1;
+				}
+
+				if let Ok(val) = num.parse::<i32>()
+				{
+					tokens.push_back(Token::new_integer_literal(info, val));
+				}
+			}
+
+			'a'..='z' | 'A'..='Z' | '_' =>
+			{
+				let mut ident = String::new();
+				ident.push(c); // include the first character
+
+				while let Some(current) = chars.peek()
+				{
+					if !(current.is_ascii_alphanumeric() || *current == '_')
+					{
+						break;
+					}
+
+					ident.push(*current);
+					chars.next();
+					column += 1;
+				}
+
+				match ident.as_str()
+				{
+					"true" => tokens.push_back(Token::new_boolean_literal(info, true)),
+					"false" => tokens.push_back(Token::new_boolean_literal(info, false)),
+					_ => tokens.push_back(Token::new_identifier(info, ident))
+				}
+			}
+
+
+			'=' =>
+			{
+				if *chars.peek().unwrap() == '='
+				{
+					tokens.push_back(Token::new_comparison(info, ComparisonOperation::IsEqual));
+					chars.next();
+					column += 1;
+				}
+				else
+				{
+					tokens.push_back(Token::new_symbol(info, Symbol::Equal));
+				}
+			}
+
+			'!' =>
+			{
+				if *chars.peek().unwrap() == '='
+				{
+					tokens.push_back(Token::new_comparison(info, ComparisonOperation::IsNotEqual));
+					chars.next();
+					column += 1;
+				}
+				else 
+				{
+					unimplemented!();
+				}
+			}
+
+			'<' =>
+			{
+				if *chars.peek().unwrap() == '='
+				{
+					tokens.push_back(Token::new_comparison(info, ComparisonOperation::IsLessOrEqual));
+					chars.next();
+					column += 1;
+				}
+				else
+				{
+					tokens.push_back(Token::new_comparison(info, ComparisonOperation::IsLess));
+				}
+			}
+
+			'>' =>
+			{
+				if *chars.peek().unwrap() == '='
+				{
+					tokens.push_back(Token::new_comparison(info, ComparisonOperation::IsGreaterOrEqual));
+					chars.next();
+					column += 1;
+				}
+				else
+				{
+					tokens.push_back(Token::new_comparison(info, ComparisonOperation::IsGreater));
+				}
+			}
+
+			'|' =>
+			{
+				if *chars.peek().unwrap() == '|'
+				{
+					tokens.push_back(Token::new_boolean(info, BooleanOperation::Or));
+					chars.next();
+					column += 1;
+				}
+			}
+
+			'(' =>
+			{
+				tokens.push_back(Token::new_symbol(info, Symbol::LeftParen));
+			}
+
+			')' =>
+			{
+				tokens.push_back(Token::new_symbol(info, Symbol::RightParen));
+			}
+
+			';' =>
+			{
+				tokens.push_back(Token::new_symbol(info, Symbol::Semicolon));
+			}
+
+			unknown =>
+			{
+				panic!("Unknown character `{unknown}`: line {line}, column {column}");
+			}
+		}
+	}
+
+	return tokens;
+}
---
+++ src/parser/mod.rs
@@ -0,0 +1,408 @@
+pub mod token;
+use token::*;
+
+pub mod lexer;
+use lexer::*;
+
+use std::collections::VecDeque;
+
+use crate::ast::root::*;
+use crate::ast::statement::*;
+use crate::ast::literal::*;
+use crate::ast::expression::*;
+
+use crate::data::syms::*;
+use crate::data::ops::*;
+use crate::data::vtype::VType;
+
+
+// Internal operator enum for shunting yard
+fn parse_expression(mut tokens: VecDeque<Token>) -> Expression
+{
+	#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+	enum Assoc
+	{
+		Left,
+		Right
+	}
+
+	#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+	enum Operator
+	{
+		Add,
+		Sub,
+		Mul,
+		Div,
+		Eq,
+		Neq,
+		Gt,
+		Gte,
+		Lt,
+		Lte,
+		And,
+		Or
+	}
+
+	fn precedence_of(op: Operator) -> (u8, Assoc)
+	{
+		use Operator::*;
+
+		match op
+		{
+			Mul | Div             => (3, Assoc::Left),
+			Add | Sub             => (2, Assoc::Left),
+			Gt | Gte | Lt | Lte |
+			Eq | Neq              => (1, Assoc::Left),
+			And                   => (0, Assoc::Left),
+			Or                    => (0, Assoc::Left)
+		}
+	}
+
+	fn apply_operator(op: Operator, stack: &mut Vec<Expression>)
+	{
+		let rhs = stack.pop().expect("Missing RHS expression");
+		let lhs = stack.pop().expect("Missing LHS expression");
+
+		use Operator::*;
+
+		match op
+		{
+			Add | Sub | Mul | Div =>
+			{
+				let arith_op = match op
+				{
+					Add => ArithmeticOperation::Add,
+					Sub => ArithmeticOperation::Subtract,
+					Mul => ArithmeticOperation::Multiply,
+					Div => ArithmeticOperation::Divide,
+					_   => unreachable!()
+				};
+
+				stack.push(Expression::new_arithmetic(
+					VType::Integer,
+					arith_op,
+					lhs,
+					rhs
+				));
+			}
+
+			Eq | Neq | Gt | Gte | Lt | Lte =>
+			{
+				let cmp_op = match op
+				{
+					Eq  => ComparisonOperation::IsEqual,
+					Neq => ComparisonOperation::IsNotEqual,
+					Gt  => ComparisonOperation::IsGreater,
+					Gte => ComparisonOperation::IsGreaterOrEqual,
+					Lt  => ComparisonOperation::IsLess,
+					Lte => ComparisonOperation::IsLessOrEqual,
+					_   => unreachable!()
+				};
+
+				stack.push(Expression::new_comparison(
+					cmp_op,
+					lhs,
+					rhs
+				));
+			}
+
+			And | Or =>
+			{
+				let bool_op = match op
+				{
+					And => BooleanOperation::And,
+					Or  => BooleanOperation::Or,
+					_   => unreachable!()
+				};
+
+				stack.push(Expression::new_boolean(
+					bool_op,
+					lhs,
+					rhs
+				));
+			}
+		}
+	}
+
+	let mut output_stack: Vec<Expression> = Vec::new();
+	let mut operator_stack: Vec<Operator> = Vec::new();
+
+	while let Some(token) = tokens.pop_front()
+	{
+		match token.get_type()
+		{
+			TokenType::IntegerLiteral =>
+			{
+				let lit = token.as_token::<IntegerLiteralToken>().unwrap().clone();
+				output_stack.push(Expression::new_literal(Literal::new_integer(lit.value())));
+			}
+
+			TokenType::BooleanLiteral =>
+			{
+				let lit = token.as_token::<BooleanLiteralToken>().unwrap().clone();
+				output_stack.push(Expression::new_literal(Literal::new_boolean(lit.value())));
+			}
+
+			TokenType::Arithmetic =>
+			{
+				let arith_token = token.as_token::<ArithmeticToken>().unwrap().clone();
+				let op = match arith_token.op()
+				{
+					ArithmeticOperation::Add      => Operator::Add,
+					ArithmeticOperation::Subtract => Operator::Sub,
+					ArithmeticOperation::Multiply => Operator::Mul,
+					ArithmeticOperation::Divide   => Operator::Div
+				};
+
+				let (prec, assoc) = precedence_of(op);
+
+				while let Some(&top_op) = operator_stack.last()
+				{
+					let (top_prec, _) = precedence_of(top_op);
+
+					let should_apply = match assoc
+					{
+						Assoc::Left => prec <= top_prec,
+						Assoc::Right => prec < top_prec
+					};
+
+					if should_apply
+					{
+						operator_stack.pop().map(|op| apply_operator(op, &mut output_stack));
+					}
+					else
+					{
+						break;
+					}
+				}
+
+				operator_stack.push(op);
+			}
+
+			TokenType::Comparison =>
+			{
+				let cmp_token = token.as_token::<ComparisonToken>().unwrap().clone();
+				let op = match cmp_token.op()
+				{
+					ComparisonOperation::IsEqual            => Operator::Eq,
+					ComparisonOperation::IsNotEqual         => Operator::Neq,
+					ComparisonOperation::IsGreater          => Operator::Gt,
+					ComparisonOperation::IsGreaterOrEqual   => Operator::Gte,
+					ComparisonOperation::IsLess             => Operator::Lt,
+					ComparisonOperation::IsLessOrEqual      => Operator::Lte
+				};
+
+				let (prec, assoc) = precedence_of(op);
+
+				while let Some(&top_op) = operator_stack.last()
+				{
+					let (top_prec, _) = precedence_of(top_op);
+
+					let should_apply = match assoc
+					{
+						Assoc::Left => prec <= top_prec,
+						Assoc::Right => prec < top_prec
+					};
+
+					if should_apply
+					{
+						operator_stack.pop().map(|op| apply_operator(op, &mut output_stack));
+					}
+					else
+					{
+						break;
+					}
+				}
+
+				operator_stack.push(op);
+			}
+
+			TokenType::Boolean =>
+			{
+				let bool_token = token.as_token::<BooleanToken>().unwrap().clone();
+				let op = match bool_token.op()
+				{
+					BooleanOperation::And => Operator::And,
+					BooleanOperation::Or  => Operator::Or
+				};
+
+				let (prec, assoc) = precedence_of(op);
+
+				while let Some(&top_op) = operator_stack.last()
+				{
+					let (top_prec, _) = precedence_of(top_op);
+
+					let should_apply = match assoc
+					{
+						Assoc::Left => prec <= top_prec,
+						Assoc::Right => prec < top_prec
+					};
+
+					if should_apply
+					{
+						operator_stack.pop().map(|op| apply_operator(op, &mut output_stack));
+					}
+					else
+					{
+						break;
+					}
+				}
+
+				operator_stack.push(op);
+			}
+
+			TokenType::Symbol =>
+			{
+				let sym_token = token.as_token::<SymbolToken>().unwrap().clone();
+
+				match sym_token.sym()
+				{
+					Symbol::LeftParen =>
+					{
+						let mut depth = 1;
+						let mut sub_tokens: VecDeque<Token> = VecDeque::new();
+
+						while let Some(next_token) = tokens.pop_front()
+						{
+							if next_token.get_type() == TokenType::Symbol
+							{
+								let sub_sym_token = next_token.as_token::<SymbolToken>().unwrap();
+
+								match sub_sym_token.sym()
+								{
+									Symbol::LeftParen => depth += 1,
+									Symbol::RightParen =>
+									{
+										depth -= 1;
+										if depth == 0
+										{
+											break;
+										}
+									}
+									_ => {}
+								}
+							}
+
+							sub_tokens.push_back(next_token);
+						}
+
+						if depth != 0
+						{
+							panic!("Unclosed parenthesis in expression");
+						}
+
+						let inner_expr = parse_expression(sub_tokens);
+						output_stack.push(inner_expr);
+					}
+
+					Symbol::RightParen =>
+					{
+						panic!("Unmatched right parenthesis");
+					}
+
+					_ => panic!("Unexpected symbol in expression: {:?}", sym_token.sym())
+				}
+			}
+
+			_ => panic!("Unexpected token in expression: {:?}", token.get_type())
+		}
+	}
+
+	while let Some(op) = operator_stack.pop()
+	{
+		apply_operator(op, &mut output_stack);
+	}
+
+	assert_eq!(output_stack.len(), 1, "Expression stack must end with exactly one value");
+
+	output_stack.pop().unwrap()
+}
+
+
+pub struct StatementReturn
+{
+	statement: Statement,
+	remaining_tokens: VecDeque<Token>,
+}
+
+pub fn parse_statement(stmt_tokens: VecDeque<Token>) -> StatementReturn
+{
+	let mut tokens = stmt_tokens.into_iter().peekable();
+
+	while let Some(t) = tokens.next()
+	{
+		match t.get_type()
+		{
+			token::TokenType::Identifier =>
+			{
+				let t = t.as_token::<IdentifierToken>().unwrap().clone();
+
+				if t.name() == "print"
+				{
+					let mut expr_tokens: VecDeque<Token> = VecDeque::new();
+
+					loop
+					{
+						let Some(next_token) = tokens.peek() else
+						{
+							break;
+						};
+
+						if next_token.get_type() == TokenType::Symbol
+						{
+							let sym_token_opt = next_token.as_token::<SymbolToken>();
+
+							if let Some(sym_token) = sym_token_opt
+							{
+								if sym_token.sym() == Symbol::Semicolon
+								{
+									tokens.next(); // consume ;
+									break;
+								}
+							}
+						}
+
+						if let Some(token) = tokens.next()
+						{
+							expr_tokens.push_back(token);
+						}
+					}
+
+					let expr = parse_expression(expr_tokens);
+
+					return StatementReturn
+					{
+						statement: Statement::new_print(expr),
+						remaining_tokens: tokens.collect()
+					}
+				}
+				else
+				{
+					panic!("Unexpected identifier `{}`", t.name());
+				}
+			}
+
+			_ => panic!("Unexpected starting statement token `{:?}`", t.get_type())
+		}
+	}
+
+	panic!();
+}
+
+pub fn parse_root(source: String) -> Root
+{
+	let tokens = lex(source);
+	let mut root = Root::new();
+
+	let mut token_queue = tokens.into_iter().collect::<VecDeque<_>>();
+
+	while !token_queue.is_empty()
+	{
+		let result = parse_statement(token_queue.clone());
+
+		root.add(result.statement);
+		token_queue = result.remaining_tokens;
+	}
+
+	root
+}
